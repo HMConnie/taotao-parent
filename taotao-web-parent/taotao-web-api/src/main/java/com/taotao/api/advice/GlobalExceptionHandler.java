591e6f28e09155e6bea1c27c941379ecec596722
HMConnie <HMConnie@sina.com>
2018-06-16 01:37:51 +0800

    修改包名

---
+++ taotao-web-parent/taotao-web-api/src/main/java/com/taotao/api/advice/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.taotao.api.advice;
+
+import com.taotao.common.exception.CustomException;
+import com.taotao.common.pojo.TaotaoResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(CustomException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public TaotaoResult handleException(CustomException e) {
+        return TaotaoResult.build(HttpStatus.FORBIDDEN.value(), e.getTips());
+    }
+}
